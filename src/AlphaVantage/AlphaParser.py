81bc828e9791759c07f915fd0237035835ef9b42
sollsollsoll <solomonchau@hotmail.com>
2020-02-02 16:32:43 -0500

    Update AlphaParser.py
    alphaparser update

--- src/AlphaVantage/AlphaParser.py
+++ src/AlphaVantage/AlphaParser.py
@@ -4,16 +4,17 @@ import json
 api_url = 'https://www.alphavantage.co/query'
 
 class AlphaParser:
-    def __init__(self,key,symbol,interval,time_period,series_type):
+
+    def __init__(self,key,symbols,interval,time_period,series_type):
         self.key = key
-        self.symbol = symbol
+        self.symbols = symbols
         self.interval = interval
         self.time_period = time_period
         self.series_type = series_type    
 
     # Just a sample from the api documentation. 
     # To use other functions, check https://www.alphavantage.co/documentation/
-    def getSMAvalue(self,company, interval, time_period, series_type):
+    def getSMAvalue(self, company, interval, time_period, series_type):
         parameters = {
             'function' : 'SMA',
             'symbol' : company,
@@ -24,4 +25,19 @@ class AlphaParser:
         }
         r = requests.get(api_url, parameters)
         return r.json()
-        
+
+    def addSymbol(self, new_symbol):
+        if len(self.symbols) < 5 and new_symbol not in self.symbols:
+            self.symbols.append(new_symbol)
+        else:
+            print("Too many symbols")
+
+    def changeInterval(self, new_interval):
+        self.interval = new_interval
+
+    def changeTimePeriod(self, new_time_period):
+        self.time_period = new_time_period
+
+    def changeSeriesType(self, new_series_type):
+        self.series_type = new_series_type
+
